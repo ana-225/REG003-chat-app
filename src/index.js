7f453690027e2924e69103f70a72129256a55a4a
ana-225 <ana.vidal.s@hotmail.com>
2021-09-15 11:38:29 -0300

    config change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
+console.log(process.env);
 app.get('/', (req, res) => {
   res.send('Hello Fares!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
