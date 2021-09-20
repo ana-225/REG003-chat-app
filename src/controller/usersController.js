b39893d5d78c5b4858d991d4f3f1251ddded5490
ana-225 <ana.vidal.s@hotmail.com>
2021-09-20 13:53:26 -0300

    Created users

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -9,6 +9,29 @@ const getUsers = async (req, res) => {
   res.status(200).json(response.rows);
 };
 
+// POST '/users'
+const createUsers = async (req, res, next) => {
+  try {
+    // const { id, name, email } = req.body;
+    const id = 2;
+    const name = 'Ana';
+    const email = 'ana@gmail.com';
+
+    if (!id || !name || !email) {
+      return next(400);
+    }
+  } catch (error) {
+    next(error);
+  }
+
+  const response = await client.query(
+    'INSERT INTO public.users ( userId, userName, userEmail) VALUES ($1, $2, $3)',
+    [id, name, email]
+  );
+  return res.status(200).json(response.rows);
+};
+
 module.exports = {
   getUsers,
+  createUsers,
 };
