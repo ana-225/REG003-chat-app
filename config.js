330d057b5aec7983838e94acc08c18fd5421d68d
ana-225 <ana.vidal.s@hotmail.com>
2021-09-22 14:57:58 -0300

    Config deployd DB Heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.port = process.env.PORT;
 exports.dbUrl =
   process.env.DATABASE ||
-  'postgres://postgres:postgres@localhost:5432/postgres';
+  'postgres://dnlhmiogrxigiy:d5324e320d3414e761264ed46bc7395ed8b72bbf2ed1eaa45d443aa7e1fe250b@ec2-3-219-111-26.compute-1.amazonaws.com:5432/df2lc0ku3mnad4';
 exports.secret = process.env.JWT_SECRET || 'token-para-chat';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'chatpassword';
