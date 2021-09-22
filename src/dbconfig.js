d995f232998c73ab5d2f0d6a7a76442550bbc2fa
ana-225 <ana.vidal.s@hotmail.com>
2021-09-22 18:41:14 -0300

    Test DB

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -3,7 +3,9 @@ const { Client } = require('pg');
 const { parse } = require('pg-connection-string').parse;
 const { dbUrl } = require('../config');
 
-const config = parse(dbUrl);
+const config = parse(
+  'postgres://dnlhmiogrxigiy:d5324e320d3414e761264ed46bc7395ed8b72bbf2ed1eaa45d443aa7e1fe250b@ec2-3-219-111-26.compute-1.amazonaws.com:5432/df2lc0ku3mnad4'
+);
 
 // conexión a base de datos
 const client = new Client(config);
