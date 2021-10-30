b052d9211c08ca38e16411e25dd14c352867b98f
ana-225 <ana.vidal.s@hotmail.com>
2021-10-30 17:24:05 -0300

    registry function review

---
+++ src/__mocks__/dbconfig.js
@@ -0,0 +1,16 @@
+// const Pgmock = require('pgmock2');
+// const { getPool } = require('pgmock2');
+
+// const pg = new Pgmock();
+// console.log('entra al mocks');
+// // Using pg instance from above examples.
+// const pool = getPool(pg);
+// pool.connect((err) => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log('BD conectada');
+//   }
+// });
+
+// module.exports = pool;
