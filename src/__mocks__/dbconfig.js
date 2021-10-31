bcca6b7923b43c6b3eca896c280be70db47604b1
ana-225 <ana.vidal.s@hotmail.com>
2021-10-30 22:14:14 -0300

    Update file

--- src/__mocks__/dbconfig.js
+++ src/__mocks__/dbconfig.js
@@ -1,16 +1,16 @@
-const Pgmock = require('pgmock2');
-const { getPool } = require('pgmock2');
+// const Pgmock = require('pgmock2');
+// const { getPool } = require('pgmock2');
 
-const pg = new Pgmock();
-console.log('entra al mocks');
-// Using pg instance from above examples.
-const pool = getPool(pg);
-pool.connect((err) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log('BD conectada');
-  }
-});
+// const pg = new Pgmock();
+// console.log('entra al mocks');
+// // Using pg instance from above examples.
+// const pool = getPool(pg);
+// pool.connect((err) => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log('BD conectada');
+//   }
+// });
 
-module.exports = pool;
+// module.exports = pool;
